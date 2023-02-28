ad05be457271610bdb5995b2411e5e6ba16774ea
Terentev Dmitry <zabey52@gmail.com>
2023-02-28 20:53:48 +0300

    v2

--- chatWithBarman.cpp
+++ chatWithBarman.cpp
@@ -11,7 +11,12 @@ void chatWithBarman(int* menuSelection)
     std::cout << "Вы должны с завязанными глазами угадать количество глотков выпивки, которую я накапаю в стакан." << std::endl;
     std::cout << "Не бойся! Буду наливать не больше десяти глотков." << std::endl;
     std::cout << "Если не угадаешь - получишь такую оплеуху от меня, что звездочки будут плясать джигу на твоём лбу!" << std::endl;
-    
+    //std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "А ещё разрешите представить - это наша красавица, Светлана! Первая леди нашего заведения.";
+    std::cout << "Она будет тебя подбадривать всю игру! " << std::endl;
+    //playSound("privetikr.ogg");
+    //std::this_thread::sleep_for(std::chrono::seconds(2));
+
     std::cout << "1. Отодвинуться" << std::endl;
     std::cout << '>';
     
